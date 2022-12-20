d32e0ee53512642818097d85719b765bfdf3dbe5
kaustin <krystalaustin99@gmail.com>
2022-12-20 14:12:15 -0600

    git init
    creating a web scraper

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://www.devmytech.com');
+    await page.screenshot({ path: 'screenshots/devmy-home.png' });
+
+    await page.goto('https://www.devmytech.com/billing');
+    await page.screenshot({ path: 'screenshots/devmy-billing.png' });
+
+    await page.goto('https://www.devmytech.com/billing/login');
+    await page.screenshot({ path: 'screenshots/devmy-billing-login.png' });
+
+
+    await browser.close();
+};
+
+run();
---
+++ script.js
@@ -0,0 +1,7 @@
+const date = new Date();
+const lastScan = document.querySelectorAll('.last-scan');
+lastScan.forEach((lastScanText) => {
+    lastScanText.innerText = 'last scan: ' + date;
+})
+
+
