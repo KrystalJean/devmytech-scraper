0cdb89e980f1ec9b5e4d311814d83152dfca481a
kaustin <krystalaustin99@gmail.com>
2024-11-06 21:43:34 -0600

    timestamp runs on start now, some style changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 
-
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
--- script.js
+++
@@ -1,7 +0,0 @@
-const date = new Date();
-const lastScan = document.querySelectorAll('.last-scan');
-lastScan.forEach((lastScanText) => {
-    lastScanText.innerText = 'last scan: ' + date;
-})
-
-
